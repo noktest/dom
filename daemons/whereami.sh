629177fa778ae1b99b4f7d2226ce74d27eb03ac4
noktest <pnorivotset@gmail.com>
2026-07-04 01:03:39 +0200

    Create whereami.sh

---
+++ daemons/whereami.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# sets timezone automatically (timedatectl)
+
+set -euo pipefail
+
+APIS=(
+  "https://ipapi.co/timezone/" # Europe/Warsaw
+  "https://ipwho.is/?fields=timezone.id,timezone.time_zone" # {"timezone":{"id":"Europe/Warsaw"}}
+  "https://free.freeipapi.com/api/json/" # {..., "timezone":{"id":"Europe/Warsaw"}, ...}
+  "https://ip-api.com" # wall of text
+)
+
+# internet?
+ping -c 3 -W 3 -w 10 8.8.8.8 > /dev/null || exit 1
+
+for url in "${APIS[@]}"; do
+   # extract the first thing that looks like iana timezone format, escape \ insensitive
+   raw=$(curl -s --max-time 10 "$url") || continue
+   timezone=$(echo "$raw" | grep -oE '[A-Z][a-z]+(\\?/([A-Z][a-z]+(_[A-Z][a-z]+)*))+' | sed 's#\\##g' | head -n1)
+   # empty?
+   [[ -n "$timezone" ]] || continue
+   # real timezone?
+   if timedatectl list-timezones | grep -Fq "$timezone"; then
+       timedatectl set-timezone "$timezone" #set
+       echo "$timezone"
+       exit 0
+   fi
+done
+# chuj ci w dupe cwelu
+echo "failed to set timezone"
+exit 1
